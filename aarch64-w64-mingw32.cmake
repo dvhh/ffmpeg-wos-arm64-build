956d3d885295927c06ff34c145786efe898e870b
dvhh <dvhh@users.noreply.github.com>
2024-10-11 22:30:22 +0900

    Update aarch64-w64-mingw32.cmake

--- aarch64-w64-mingw32.cmake
+++ aarch64-w64-mingw32.cmake
@@ -6,6 +6,13 @@ set(CMAKE_SYSTEM_NAME Windows)
 # which compilers to use for C and C++
 set(CMAKE_C_COMPILER   aarch64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER aarch64-w64-mingw32-g++)
+SET(CMAKE_RC_COMPILER aarch64-w64-mingw32-windres)
+
+
+SET(CMAKE_CXX_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
+SET(CMAKE_C_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
+SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
+SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
 
 # where is the target environment located 
 # set(CMAKE_FIND_ROOT_PATH  /usr/i586-mingw32msvc
