a28b4d8c3a484205fee011e735486d78c319ac89
dvhh <dvhh@users.noreply.github.com>
2024-10-13 00:27:16 +0900

    Update build-sdl2.sh

--- scripts/build-sdl2.sh
+++ scripts/build-sdl2.sh
@@ -6,6 +6,6 @@ OUTPUT="${1}"
 mkdir -p "${OUTPUT}"
 mkdir build
 cd build
-../sdl/configure --host=aarch64-w64-mingw32 --prefix="${OUTPUT}"
+../sdl2/configure --host=aarch64-w64-mingw32 --prefix="${OUTPUT}"
 make -j16
 make install
