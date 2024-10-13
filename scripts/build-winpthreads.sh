d1376070a74bf5122c67c412d0519e0856ae91b8
dvhh <dvhh@users.noreply.github.com>
2024-10-13 14:41:34 +0000

    winpthreads

---
+++ scripts/build-winpthreads.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+OUTPUT="${1}"
+HOST="${2}"
+mkdir -p "${OUTPUT}"
+
+git clone https://git.code.sf.net/p/mingw-w64/mingw-w64.git" mingw-w64
+pushd mingw-w64/mingw-w64-libraries/winpthreads
+./configure --host="${HOST}" --prefix="${OUTPUT}" --with-pic --enable-shared --enable-static
+make -j16
+make install
