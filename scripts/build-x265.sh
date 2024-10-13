133452eda65c60aa2933cc1406172ebd9b553bad
dvhh <dvhh@users.noreply.github.com>
2024-10-13 14:45:55 +0000

    revert depth

--- scripts/build-x265.sh
+++ scripts/build-x265.sh
@@ -8,7 +8,7 @@ mkdir -p "${OUTPUT}"
 apt update
 apt install -qy cmake yasm cmake-curses-gui
 ln -s "/opt/llvm-mingw/bin/${HOST}-windres" /bin/windres
-git clone https://bitbucket.org/multicoreware/x265_git.git x265 -b stable --depth 1
+git clone https://bitbucket.org/multicoreware/x265_git.git x265 -b stable
 cd x265/build/linux
 export PKG_CONFIG_LIBDIR="${OUTPUT}/lib/pkgconfig"
 cmake -DCMAKE_TOOLCHAIN_FILE="${GITHUB_WORKSPACE}/${HOST}.cmake" -DCMAKE_INSTALL_PREFIX="${OUTPUT}" -DCMAKE_BUILD_TYPE=Release -DCMAKE_ASM_NASM_FLAGS=-w-macro-params-legacy ../../source
