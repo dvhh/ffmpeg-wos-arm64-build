e5649f636e7edd3c893b97a27bcb4def3961f31e
dvhh <dvhh@users.noreply.github.com>
2024-10-13 15:59:32 +0000

    aom

---
+++ scripts/build-aom.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+OUTPUT="${1}"
+HOST="${2}"
+mkdir -p "${OUTPUT}"
+git clone https://aomedia.googlesource.com/aom aom
+mkdir build
+cd build
+export PKG_CONFIG_LIBDIR="${OUTPUT}/lib/pkgconfig"
+cmake -DCMAKE_TOOLCHAIN_FILE="${GITHUB_WORKSPACE}/${HOST}.cmake" -DCMAKE_INSTALL_PREFIX="${OUTPUT}" -DCMAKE_BUILD_TYPE=Release -DENABLE_EXAMPLES=NO -DENABLE_TESTS=NO -DENABLE_TOOLS=NO -DCONFIG_TUNE_VMAF=1 ../aom
+make -j16
+make install
