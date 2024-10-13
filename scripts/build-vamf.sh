93282580aef2e84e4ef21fb5305cde8648c7a68e
dvhh <dvhh@users.noreply.github.com>
2024-10-13 13:41:37 +0000

    vamf

---
+++ scripts/build-vamf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+OUTPUT="${1}"
+HOST="${2}"
+mkdir -p "${OUTPUT}"
+apt update
+apt install -qy ninja-build nasm
+meson setup --cross-file "${GITHUB_WORKSPACE}/${HOST}.txt" --prefix "${OUTPUT}" libvmaf libvmaf/build --buildtype release
+ninja -vC libvmaf/build install
