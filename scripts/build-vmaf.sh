23b196c99f1839774135d8576ed87eed15a1d10c
dvhh <dvhh@users.noreply.github.com>
2024-10-13 13:57:31 +0000

    fix vmaf build

--- scripts/build-vmaf.sh
+++ scripts/build-vmaf.sh
@@ -7,5 +7,6 @@ HOST="${2}"
 mkdir -p "${OUTPUT}"
 apt update
 apt install -qy meson ninja-build nasm
+pushd vmaf
 meson setup --cross-file "${GITHUB_WORKSPACE}/${HOST}.txt" --prefix "${OUTPUT}" libvmaf libvmaf/build --buildtype release
 ninja -vC libvmaf/build install
