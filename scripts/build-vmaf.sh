c70a6a7772ff98ff5a686b5215661f9f56c95633
dvhh <dvhh@users.noreply.github.com>
2024-10-13 13:50:19 +0000

    install mson

--- scripts/build-vmaf.sh
+++ scripts/build-vmaf.sh
@@ -6,6 +6,6 @@ OUTPUT="${1}"
 HOST="${2}"
 mkdir -p "${OUTPUT}"
 apt update
-apt install -qy ninja-build nasm
+apt install -qy meson ninja-build nasm
 meson setup --cross-file "${GITHUB_WORKSPACE}/${HOST}.txt" --prefix "${OUTPUT}" libvmaf libvmaf/build --buildtype release
 ninja -vC libvmaf/build install
