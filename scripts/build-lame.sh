1f92f69da5f31b5c5038724d4602c88d7cbb57ec
dvhh <dvhh@users.noreply.github.com>
2024-10-14 08:54:18 +0000

    test changes

--- scripts/build-lame.sh
+++ scripts/build-lame.sh
@@ -9,7 +9,7 @@ HOST="${2}"
 mkdir -p "${OUTPUT}"
 
 cd lame
-export CFLAGS="$CFLAGS -DNDEBUG -Wno-error=incompatible-pointer-types"
+# export CFLAGS="$CFLAGS -DNDEBUG -Wno-error=incompatible-pointer-types"
 ./configure --host="${HOST}" --prefix="${OUTPUT}" --with-pic --enable-nasm --disable-gtktest --disable-cpml --disable-frontend
 make -j16
 make install
