0e26af7f97f4aec1e9326d84f1e980c0e7d1bf24
dvhh <dvhh@users.noreply.github.com>
2024-10-14 04:43:42 +0000

    pkgconfig dir

--- scripts/build-libass.sh
+++ scripts/build-libass.sh
@@ -1,8 +1,15 @@
-c
+#!/bin/bash
+
+set -euxo pipefail
+
+OUTPUT="${1}"
+HOST="${2}"
+mkdir -p "${OUTPUT}"
 #mkdir build
 #cd build
 cd libass
 ./autogen.sh
+export PKG_CONFIG_LIBDIR="${OUTPUT}/lib/pkgconfig"
 ./configure --host="${HOST}" --prefix="${OUTPUT}" --with-pic
 make -j16
 make install
