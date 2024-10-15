91cec745f859f075e5ef05f5fa93f0356a2a67e7
dvhh <dvhh@users.noreply.github.com>
2024-10-15 14:40:59 +0000

    vpx

---
+++ scripts/build-vpx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+OUTPUT="${1}"
+HOST="${2}"
+mkdir -p "${OUTPUT}"
+git clone https://chromium.googlesource.com/webm/libvpx --depth 1 -b v1.14.1 vpx
+cd vpx
+./autogen.sh
+export PKG_CONFIG_LIBDIR="${OUTPUT}/lib/pkgconfig"
+
+TARGET="$(grep "${HOST}" "${GITHUB_WORKSPACE}/hosts/host2target.txt" | cut -d " " -f 2)"
+./configure --target="${TARGET}" --prefix="${OUTPUT}" --with-pic --enable-pic --disable-examples --disable-tools --disable-tools --disable-tools --disable-tools
+make -j16
+make install
