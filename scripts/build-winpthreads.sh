b8a67b2391b52800cd544b7615a0349aab8b732c
dvhh <dvhh@users.noreply.github.com>
2024-10-15 15:38:57 +0000

    shellcheck

--- scripts/build-winpthreads.sh
+++ scripts/build-winpthreads.sh
@@ -5,7 +5,8 @@ HOST="${2}"
 mkdir -p "${OUTPUT}"
 
 git clone "https://git.code.sf.net/p/mingw-w64/mingw-w64.git" mingw-w64 --depth 1
-pushd mingw-w64/mingw-w64-libraries/winpthreads
+# shellcheck disable=SC2164  
+cd mingw-w64/mingw-w64-libraries/winpthreads
 ./configure --host="${HOST}" --prefix="${OUTPUT}" --with-pic --enable-shared --enable-static
 make -j16
 make install
