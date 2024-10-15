a04c60029d6ee26d0799736d3b88586e95854aeb
dvhh <dvhh@users.noreply.github.com>
2024-10-15 14:21:27 +0000

    pkg config

--- scripts/build-theora.sh
+++ scripts/build-theora.sh
@@ -7,6 +7,7 @@ HOST="${2}"
 mkdir -p "${OUTPUT}"
 cd theora
 ./autogen.sh
+export PKG_CONFIG_LIBDIR="${OUTPUT}/lib/pkgconfig"
 ./configure --host="${HOST}" --prefix="${OUTPUT}" --with-pic --disable-extra-programs --disable-examples --disable-examples --disable-examples --disable-examples --disable-doc
 make -j16
 make install
