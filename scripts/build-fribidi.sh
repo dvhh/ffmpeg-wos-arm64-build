65ac5d52dc552594f1d42faceb4e7230488e44ce
dvhh <dvhh@users.noreply.github.com>
2024-11-07 00:05:46 +0900

    use meson

--- scripts/build-fribidi.sh
+++ scripts/build-fribidi.sh
@@ -5,8 +5,21 @@ set -euxo pipefail
 OUTPUT="${1}"
 HOST="${2}"
 mkdir -p "${OUTPUT}"
+apt update
+apt install -qy meson ninja-build nasm
 cd fribidi
-./autogen.sh
-./configure --host="${HOST}" --prefix="${OUTPUT}" --with-pic --disable-example
-make -j16
-make install
+# ./autogen.sh
+# ./configure --host="${HOST}" --prefix="${OUTPUT}" --with-pic --disable-example
+# make -j16
+# make install
+meson setup \
+  --cross-file "${GITHUB_WORKSPACE}/hosts/${HOST}.txt" \
+  --includedir "${OUTPUT}/include" \
+  --libdir "${OUTPUT}/lib" \
+  --prefix "${OUTPUT}" \
+  --buildtype=release \
+  -Dbin=false \
+  -Ddocs=false \
+  -Dtests=false 
+ninja -j$(nproc)
+ninja install
