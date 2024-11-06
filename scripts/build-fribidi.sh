607aab0256334b8912843047c74d4e19ec27dd0c
dvhh <dvhh@users.noreply.github.com>
2024-11-06 23:51:12 +0900

    typo

--- scripts/build-fribidi.sh
+++ scripts/build-fribidi.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 OUTPUT="${1}"
 HOST="${2}"
 mkdir -p "${OUTPUT}"
-cd fibidi
+cd fribidi
 ./autogen.sh
 ./configure --host="${HOST}" --prefix="${OUTPUT}" --with-pic --disable-example
 make -j16
